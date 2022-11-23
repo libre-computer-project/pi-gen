b4f572ffb005e597fb07d950257862df3448dd86
Da Xue <da@lessconfused.com>
2022-11-23 04:44:38 -0500

    libre-computer: download gpg from github and add to apt trusted

--- stage0/00-configure-apt/00-run.sh
+++ stage0/00-configure-apt/00-run.sh
@@ -4,6 +4,7 @@ install -m 644 files/sources.list "${ROOTFS_DIR}/etc/apt/"
 install -m 644 files/raspi.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
 sed -i "s/RELEASE/${RELEASE}/g" "${ROOTFS_DIR}/etc/apt/sources.list"
 sed -i "s/RELEASE/${RELEASE}/g" "${ROOTFS_DIR}/etc/apt/sources.list.d/raspi.list"
+install -m 644 files/libre-computer-deb.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
 
 if [ -n "$APT_PROXY" ]; then
 	install -m 644 files/51cache "${ROOTFS_DIR}/etc/apt/apt.conf.d/51cache"
@@ -14,6 +15,10 @@ fi
 
 cat files/raspberrypi.gpg.key | gpg --dearmor > "${STAGE_WORK_DIR}/raspberrypi-archive-stable.gpg"
 install -m 644 "${STAGE_WORK_DIR}/raspberrypi-archive-stable.gpg" "${ROOTFS_DIR}/etc/apt/trusted.gpg.d/"
+
+wget -O "${STAGE_WORK_DIR}/libre-computer-deb.gpg" 'https://github.com/libre-computer-project/libretech-keyring/raw/refs/heads/master/libre-computer-deb.gpg'
+install -m 644 "${STAGE_WORK_DIR}/libre-computer-deb.gpg" "${ROOTFS_DIR}/etc/apt/trusted.gpg.d/"
+
 on_chroot <<- \EOF
 	ARCH="$(dpkg --print-architecture)"
 	if [ "$ARCH" = "armhf" ]; then
