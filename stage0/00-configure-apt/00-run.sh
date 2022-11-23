fc22bb8b5721636fd4f05c3d6eb09daf1209faba
Da Xue <da@lessconfused.com>
2022-11-23 04:44:38 -0500

    libre-computer: add gpg to apt trusted

--- stage0/00-configure-apt/00-run.sh
+++ stage0/00-configure-apt/00-run.sh
@@ -4,6 +4,7 @@ install -m 644 files/sources.list "${ROOTFS_DIR}/etc/apt/"
 install -m 644 files/raspi.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
 sed -i "s/RELEASE/${RELEASE}/g" "${ROOTFS_DIR}/etc/apt/sources.list"
 sed -i "s/RELEASE/${RELEASE}/g" "${ROOTFS_DIR}/etc/apt/sources.list.d/raspi.list"
+install -m 644 files/libre-computer-deb.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
 
 if [ -n "$APT_PROXY" ]; then
 	install -m 644 files/51cache "${ROOTFS_DIR}/etc/apt/apt.conf.d/51cache"
@@ -14,6 +15,9 @@ fi
 
 cat files/raspberrypi.gpg.key | gpg --dearmor > "${STAGE_WORK_DIR}/raspberrypi-archive-stable.gpg"
 install -m 644 "${STAGE_WORK_DIR}/raspberrypi-archive-stable.gpg" "${ROOTFS_DIR}/etc/apt/trusted.gpg.d/"
+
+cat files/libre-computer-deb.gpg > "${ROOTFS_DIR}/etc/apt/trusted.gpg.d/libre-computer-deb.gpg"
+
 on_chroot << EOF
 dpkg --add-architecture armhf
 apt-get update
