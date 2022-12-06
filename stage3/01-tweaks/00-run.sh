a66639b3b13f943375f2a06c7ccc1195bef8c47b
Da Xue <da.xue@libretech.co>
2022-12-05 21:43:45 -0500

    libre-computer: add x11 tweaks

--- stage3/01-tweaks/00-run.sh
+++ stage3/01-tweaks/00-run.sh
@@ -3,3 +3,10 @@
 on_chroot << EOF
 	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_boot_wait 1
 EOF
+
+install -d				"${ROOTFS_DIR}/usr/share/X11/xorg.conf.d"
+install -m 644 files/10-disable-glx.conf		"${ROOTFS_DIR}/usr/share/X11/xorg.conf.d/"
+install -m 644 files/10-meson.conf		"${ROOTFS_DIR}/usr/share/X11/xorg.conf.d/"
+install -m 644 files/10-rockchip.conf		"${ROOTFS_DIR}/usr/share/X11/xorg.conf.d/"
+install -m 644 files/10-sunxi.conf		"${ROOTFS_DIR}/usr/share/X11/xorg.conf.d/"
+
