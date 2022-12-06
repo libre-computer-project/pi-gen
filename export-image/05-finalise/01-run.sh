555f84810e8e419ad8893c750417569552f32203
Da Xue <da.xue@libretech.co>
2022-12-06 17:52:23 -0500

    libre-computer: update initramfs and install grub to final image

--- export-image/05-finalise/01-run.sh
+++ export-image/05-finalise/01-run.sh
@@ -12,6 +12,9 @@ fi
 if hash hardlink 2>/dev/null; then
 	hardlink -t /usr/share/doc
 fi
+update-initramfs -k all -c
+grub-install --efi-directory=/boot --force-extra-removable --no-nvram --no-uefi-secure-boot
+update-grub
 EOF
 
 if [ -f "${ROOTFS_DIR}/etc/initramfs-tools/update-initramfs.conf" ]; then
